47cc98c6acd8a92c35ed069896b32eef8a717bbd
Alyssa Ruefenacht <aruefenacht14@gmail.com>
2020-05-15 05:56:56 +1000

    notepad JS- added delete and toggle functions

--- VanillaJS Notepad/Notepad-script.js
+++ VanillaJS Notepad/Notepad-script.js
@@ -2,46 +2,20 @@ var button = document.getElementById("addMe");
 var userInput = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
-// button.addEventListener("click", function(){
-//     if (userInput.value.length > 0) {
-//     var li = document.createElement("li");
-//     li.appendChild(document.createTextNode(userInput.value));
-//     ul.appendChild(li);
-//     userInput.value = "";
-//     }
-// })
-
-//createElement will create the element in quotes but in order to actually display text within it, you have to use createTextNode. Even here, you will not see the display because it isn't attached to anything. That is why we attach the li to the ul.
-
-//the if element tells us to only run the code if there is something typed.
-//the userInput.value set equal to an empty string is so that there isn't anything in the input after being clicked.
-
-// userInput.addEventListener("keypress", function(event){
-//     if (userInput.value.length > 0 && event.which === 13 || event.keyCode === 13) {
-//     var li = document.createElement("li");
-//     li.appendChild(document.createTextNode(userInput.value));
-//     ul.appendChild(li);
-//     userInput.value = "";
-//     }
-// })
-
-//this looks for the '13' key (enter) to be pressed while in the userInput field and will add it to the list. check out https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes to look at more keycodes.
-
-//one issue here: there is a lot of repeating code. How do we clean it up? Look at the repeating code and set it to a function!
-
-//minor change
 function inputLength() {
     return userInput.value.length;
 }
-
-//big change
 function createListElement () {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(userInput.value));
     ul.appendChild(li);
     userInput.value = "";
-    }
 
+        var btn = document.createElement("button");
+        btn.appendChild(document.createTextNode("Delete!"));
+        li.appendChild(btn);
+        btn.onclick = removeParent;
+    }
 function addAfterClick() {
     if (inputLength() > 0) {
         createListElement();
@@ -55,5 +29,33 @@ function addAfterEnter (event) {
 
 //look how clean!
 button.addEventListener("click", addAfterClick);
-
 userInput.addEventListener("keypress", addAfterEnter);
+
+//DOM EVENT EXERCISE
+
+//toggle done class
+var listItems = document.getElementsByTagName("li");
+
+ul.onclick = function(event) {
+    var target = event.target;
+    target.classList.toggle("done");
+}
+
+//DELETE BUTTONS
+
+function listLength() {
+    return listItems.length;
+}
+function removeParent(event) {
+    event.target.parentNode.remove();
+}
+function deleteButton() {
+    var btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("Delete!"));
+    listItems[i].appendChild(btn);
+    btn.onclick = removeParent;
+}
+    
+for (i=0;i<listLength();i++){
+    deleteButton();
+}
