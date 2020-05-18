73cfe26f1573232d045d5de8b8410059b679e2fc
Alyssa Ruefenacht <aruefenacht14@gmail.com>
2020-05-19 06:56:07 +1000

    randomized colors on load and added random button

--- BG-Gradient/BGG-script.js
+++ BG-Gradient/BGG-script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomButton = document.querySelector(".random");
 
 function colorInput () {
     body.style.background = 
@@ -14,12 +15,29 @@ function colorInput () {
     css.textContent = body.style.background + ";";
 }
 
+//you can select an input event by "input"
 color1.addEventListener("input", colorInput);
 
 color2.addEventListener("input", colorInput);
 
-//you can select an input event by "input"
+randomButton.addEventListener("click", setRandomColor);
 
-function getRandomColor() {
+// function getRandomColor() {
     
-}
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+      
+      function setRandomColor(){
+          color1.value = getRandomColor(); 
+          color2.value = getRandomColor();
+          colorInput();
+      }
+      
+//Window will have textContent onload
+window.addEventListener(document.onload, setRandomColor());
