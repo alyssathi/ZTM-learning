dcade427aa080201df2a5660480a9c568e780c3f
Alyssa Ruefenacht <aruefenacht14@gmail.com>
2020-05-21 14:16:49 +1000

    more JS practice with BG

--- VanillaJS Notepad/Notepad-script1.js
+++ VanillaJS Notepad/Notepad-script1.js
@@ -2,53 +2,6 @@ var button = document.getElementById("addMe");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
-function createListElement(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value="";
+//What do I want to do. I want to be able to click the add me button to add the input to the bottom of the list or use the enter button. I also want to be able to cross it out and toggle it by clicking the item and be able to delete things as needed. 
 
-    var btn = document.createElement("button");
-    btn.appendChild(document.createTextNode("Delete!"));
-    li.appendChild(btn);
-    btn.onclick = removeParent;
-    }
-
-function addAfterClick() {
-    if (input.value.length>0){
-        createListElement();
-    }
-}
-
-function addAfterEnter() {
-    if (input.value.length>0 && event.which===13) {
-        createListElement();
-    }
-}
-
-button.addEventListener("click", addAfterClick);
-input.addEventListener("keypress", addAfterEnter);
-
-//toggle .done class
-
-ul.onclick = function(event) {
-    event.target.classList.toggle("done");
-}
-
-//delete buttons
-var listItems = document.getElementsByTagName("li");
-
-function removeParent(){
-    event.target.parentNode.remove();
-}
-
-function createDeleteButton() {
-    var btn = document.createElement("button");
-    btn.appendChild(document.createTextNode("Delete!"));
-    listItems[i].appendChild(btn);
-    btn.onclick = removeParent;
-}
-
-for (i=0;i<listItems.length; i++){
-    createDeleteButton();
-}
+//add input to bottom of the list
